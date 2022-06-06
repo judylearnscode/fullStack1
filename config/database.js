9817f3a87cdd9bab247f1c5801633f4f7d81bf3e
Judy Chong <judyjchong@gmail.com>
2022-06-05 20:12:44 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://judychong:sp00kymuld3r@cluster0.dnbqu.mongodb.net/happyTracker?retryWrites=true&w=majority', 
+    'dbName': 'happyTracker'
+};
